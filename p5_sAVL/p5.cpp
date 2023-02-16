bd86d47a0c84a1edb7401d3d530c4c1947260212
Hangry-netizen <hangrylydevour@gmail.com>
2023-02-16 17:23:33 -0600

    initialized p5_sAVL

---
+++ p5_sAVL/p5.cpp
@@ -0,0 +1,273 @@
+/*
+* Joseph Song
+* Queena Lee
+* CSC255 Spring 2023
+* Assignment: Program 5a
+*/
+
+#include <iostream>
+#include <fstream>
+
+#include "p5.h"
+
+using namespace std;
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+sNode::sNode(string text) {
+    //constructor that causes the object node to be initialized
+    this->text = text;
+    left = right = NULL;
+    h = 1;
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+sAVL::sAVL() {
+    //constructor that causes the object tree to be initialized
+    root = NULL;
+    treeCount = 0;
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+sAVL::~sAVL() {
+    //deconstructor that deletes all the nodes
+    //in preparation for the tree to be destroyed
+    clear(root);
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+string sAVL::findMin(sNode *p) {
+    string rc = p->text;
+
+    //returns the minimum value in the subtree
+    //whose node is pointed to by pointer
+    if (p->left) {
+        rc = findMin(p->left);
+    }
+
+    return rc;
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+bool sAVL::insert(sNode *&p, string text) {
+    bool rc = false;
+
+    if (p) {
+        //if pointer exists
+        if (text < p->text) {
+            //if text is less than pointer's text, travel left
+            rc = insert(p->left, text);
+        } 
+        else if (text > p->text) {
+            //if text is more than pointer's text, travel right
+            rc = insert(p->right, text);
+        }
+    } else {
+        //otherwise, we are at a leaf and we will insert the new node here
+        p = new sNode(text);
+        treeCount++;
+        rc = true;
+    }
+
+    return rc;
+}
+
+//******************************************************************************
+//P4b Joseph Song & Queena Lee
+bool sAVL::remove(sNode *&p, string text) {
+    bool rc = false;
+
+    if (p) {
+        //if pointer exists, check if text is > or < than pointer's text
+        if (text < p->text) {
+            rc = remove(p->left, text);
+        } else if (text > p->text) {
+            rc = remove(p->right, text);
+        } else {
+            //if text is equivalent to pointer's text, check for right child
+            if (p->right) {
+                //if right child exists
+                //get the min of right child to replace the node to be removed
+                p->text = findMin(p->right);
+                rc = remove(p->right, p->text);
+            } else {
+                //otherwise, if only left child exists
+                //copy left child to replace the node to be removed
+                sNode *temp = p;
+                p = p->left;
+                delete temp;
+                treeCount--;
+                rc = true;
+            }
+            bal(p);
+            p->h = calcHeight(p);
+        }
+    }
+
+    return rc;
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+bool sAVL::isIn(sNode *p, string text) const {
+    bool rc = false;
+
+    if (p) {
+        //if pointer exists, check if text in pointer is equivalent to text
+        //if text value in pointer is larger than text
+        //traverse the left children of the pointer
+        //if text value in pointer is smaller than text
+        //traverse the right children of the pointer
+        if (text < p->text) {
+            rc = isIn(p->left, text);
+        } else if (text > p->text) {
+            rc = isIn(p->right, text);
+        } else {
+            rc = true;
+        }
+    } 
+    return rc; 
+}
+
+//******************************************************************************
+//AVL
+void sAVL::printIt(sNode *p, int &index) const {
+    if (p) {
+        printIt(p->left, p->h);
+        cout << p->text << <<endl;
+        printIt(p->right, p->h);
+    }
+}
+
+//******************************************************************************
+//P4b Joseph Song & Queena Lee
+void sAVL::clear(sNode *p) {
+
+    //if pointer exists, transverse the tree
+    //and clear nodes from right to left
+    if (p) {
+        clear(p->left);
+        clear(p->right);
+        delete p;
+    }
+}
+
+//******************************************************************************
+//AVL Done
+void sAVL::rotateLeft(sNode *&p1) {
+    sNode *p2 = p1->right;
+    p1->right = p2->left;
+    p2->left = p1;
+    p1->h = calcHeight(p1);
+    p2->h = calcHeight(p2);
+    p1 = p2;
+}
+
+//******************************************************************************
+//AVL Done
+void sAVL::rotateRight(sNode *&p1) {
+    sNode *p2 = p1->left;
+    p1->left = p2->right;
+    p2->right = p1;
+    p1->h = calcHeight(p1);
+    p2->h = calcHeight(p2);
+    p1 = p2;
+}
+
+//******************************************************************************
+//AVL Done
+void sAVL::bal(sNode *&p1) {
+    if(p1) {
+        int diff = height(p1->left) - height(p1->right);
+
+        if (diff == 2) {
+            //Left heavy
+            diff = height(p1->left->left) - height(p1->left->right);
+            if (diff == 0) {
+                //Left-right heavy
+                rotateLeft(p1->left);
+            }
+            rotateRight(p1);
+        } else if (diff == -2) {
+            //Right heavy
+            if (height(p1->right->left) > height(p1->right->right)) {
+                //Right-left heavy
+                rotateRight(p1->right);
+            }
+            rotateLeft(p1);
+        }
+        p1->h = calcHeight(p1);
+    }
+
+}
+
+//******************************************************************************
+//AVL
+int sAVL::height(sNode *p) {
+    int rc = 0;
+
+    if (p) {
+        rc = p->h;
+    }
+    return rc;
+}
+
+//******************************************************************************
+//AVL
+int sAVL::calcHeight(sNode *p) {
+    return max(height(p->left), height(p->right)) + 1;
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+bool sAVL::insert(string text) {
+    //call insert's recursion helper
+    //results in true if text is inserted
+    //otherwise, results in false
+    return insert(root, text);
+}
+
+//******************************************************************************
+//P4b Joseph Song & Queena Lee
+bool sAVL::remove(string text) {
+    //call remove's recursion helper
+    //results in true if text is removed
+    //otherwise, results in false
+    return remove(root, text);
+}
+
+//******************************************************************************
+//P4b Joseph Song & Queena Lee
+bool sAVL::isIn(string text) const {
+    //call isIn's recursion helper
+    //results in true if text is in the tree
+    //otherwise, results in false
+    return isIn(root, text);
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+void sAVL::printIt() const {
+    //results in the BST's text values being printed in ascending order
+    printIt(root, root->h);
+}
+
+//******************************************************************************
+//Joseph Song & Queena Lee
+int sAVL::count() const {
+    //returns the number of nodes in the tree
+    return treeCount;
+}
+
+//******************************************************************************
+//P4b Joseph Song & Queena Lee
+void sAVL::clear() {
+    //removes all nodes from the BST, making the BST empty
+    clear(root);
+    root = NULL;
+    treeCount = 0;
+}
---
+++ p5_sAVL/p5.h
@@ -0,0 +1,99 @@
+/*
+* Joseph Song
+* Queena Lee
+* CSC255 Spring 2023
+* Assignment: Program 5a
+*/
+
+#ifndef __P5_H
+#define __P5_H
+
+#include <string>
+
+class sNode {
+    private:
+        //holds the node's key
+        std::string text;
+
+        //for future use
+        int h;
+
+        //left and right children pointers
+        sNode *left, *right;
+
+        //node constructor
+        sNode(std::string text = "");
+        
+        friend class sAVL;
+};
+
+class sAVL {
+    private:
+        //records the number of entries in the BST
+        int treeCount;
+
+        //to point to the tree of nodes
+        sNode *root;
+
+        //returns the minimum value in the subtree
+        //whose root node is pointed to by ptr
+        std::string findMin(sNode *p);
+
+        //recursion helpers
+        bool insert(sNode *&p, std::string text);
+        bool remove(sNode *&p, std::string text);
+        bool isIn(sNode *p, std::string text) const;
+        void printIt(sNode *p, int &index) const;
+        void clear(sNode *p);
+
+        //rotateLeft when p1 is a node that is right heavy imbalanced
+        void rotateLeft(sNode *&p1);
+
+        //rotateRight when p1 is a node that is left heavy imbalanced
+        void rotateRight(sNode *&p1);
+
+        //to balance pointer
+        void bal(sNode * &p1);
+
+        //returns the height of p if it is real
+        //otherwise 0 if it is NULL
+        int height(sNode *p);
+
+        ////return the calculation of p's height
+        //based on the height of its children
+        //this is not a recursive call
+        int calcHeight(sNode *p);
+
+    public:
+        //constructor that causes object to be initialized
+        sAVL();
+
+        //destructor that deletes all the nodes in the BST
+        //in preparation for the BST to be destroyed
+        ~sAVL();
+
+        //inserts the text into the tree
+        //returns true for success
+        //returns false if the text value is already in the tree
+        bool insert(std::string text);
+
+        //removes the node with that text, deleting the node
+        //returns true for success
+        //returns false if the text is already in the tree
+        bool remove(std::string text);
+
+        //returns true if the node with text value is in the tree
+        //otherwise returns false
+        bool isIn(std::string text) const;
+
+        //print the height of each node and its "index"
+        void printIt() const;
+
+        //returns the number of nodes in the tree
+        int count() const;
+
+        //removes all nodes from the BST, making the BST empty
+        void clear();
+};
+
+#endif
